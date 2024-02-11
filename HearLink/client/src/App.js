7d5550828f880b066dad5f39effcf7cb41a6d778
Kharisara <kesara.harisara28@gmail.com>
2024-02-11 12:28:24 +0530

    Add components

--- HearLink/client/src/App.js
+++ HearLink/client/src/App.js
@@ -1,11 +1,23 @@
+
 import React from 'react';
+import { Typography, AppBar } from '@material-ui/core';
+
+import Videoplayer from './components/Videoplayer';
+import Notifications from './components/Notifiactions';
+import Options from './components/Options';
 
 const App = () => {
-    return(
-        <div> HearLink </div>
-        
+    return (
+        <div>
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Hear Link</Typography>
+            </AppBar>
+            <Videoplayer />
+            <Options>
+                <Notifications /> {/* Use Notifications component */}
+            </Options>
+        </div>
     );
-
 }
 
 export default App;
