9062f478696fac323d1cba7474b4b0ef552ed227
sahanyaL <132870882+sahanyaL@users.noreply.github.com>
2024-02-24 11:33:06 +0530

    add Agora token builder package

--- Backend/main.js
+++ Backend/main.js
@@ -1,10 +1,12 @@
 const APP_ID = "352d6ad86462494d904afc4cfeeda64b"
 
-const TOKEN = "007eJxTYGj55MT17KWelElHG7c5j8Bu/XWnONY/yWn03sUkl3dY8bACg7GpUYpZYoqFmYmZkYmlSYqlgUliWrJJclpqakqimUkSw7krqQ2BjAyT1a8zMEIhiM/CkJuYmcfAAACywh3u"
+//const TOKEN = "007eJxTYGj55MT17KWelElHG7c5j8Bu/XWnONY/yWn03sUkl3dY8bACg7GpUYpZYoqFmYmZkYmlSYqlgUliWrJJclpqakqimUkSw7krqQ2BjAyT1a8zMEIhiM/CkJuYmcfAAACywh3u"
 
-const CHANNEL = "main"
+//const CHANNEL = "main"
 
-
+//
+const TOKEN_URL = 'http://localhost:3000/token';
+//
 
 const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
 
@@ -14,7 +16,7 @@ let remoteUsers = {}
 
 
 
-let joinAndDisplayLocalStream = async (roomId) => {
+/*let joinAndDisplayLocalStream = async (roomId) => {
 
     
     client.on('user-published', handleUserJoined)
@@ -38,17 +40,48 @@ let joinAndDisplayLocalStream = async (roomId) => {
 }
 
 /*let joinStream = async () => {
+    // meka mehemama thiyanna original eke comment ekk widiyatama
     await joinAndDisplayLocalStream()
     document.getElementById('join-btn').style.display = 'none'
     document.getElementById('stream-controls').style.display = 'flex'
     
 }*/
-let joinStream = async () => {
+/*let joinStream = async () => {
     const roomId = document.getElementById('room').value.toUpperCase(); // Get the room name from input field
     await joinAndDisplayLocalStream(roomId);
     document.getElementById('join-btn').style.display = 'none';
     document.getElementById('stream-controls').style.display = 'flex';
-}
+}*/
+
+async function joinAndDisplayLocalStream(channelName, userId) {
+    client.on('user-published', handleUserJoined);
+    client.on('user-left', handleUserLeft);
+  
+    const token = await fetch(TOKEN_URL, {
+      method: 'GET',
+      query: { channelName, userId }
+    }).then(response => response.json());
+  
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+  
+    const player = `<div class="video-container" id="user-container-${userId}">
+      <div class="video-player" id="user-${userId}"></div>
+    </div>`;
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player);
+  
+    localTracks[1].play(`user-${userId}`);
+  
+    await client.publish([localTracks[0], localTracks[1]], { channel: channelName });
+  }
+  
+  async function joinStream() {
+    const roomName = document.getElementById('room').value.toUpperCase();
+    const userId = roomName; // Replace with your preferred identifier logic
+    await joinAndDisplayLocalStream(`meeting_${roomName}`, userId);
+    document.getElementById('join-btn').style.display = 'none';
+    document.getElementById('stream-controls').style.display = 'flex';
+  }
+  
 
 
 
