311c5b2b09099364c9bf477f358a15edaf046737
Kharisara <kesara.harisara28@gmail.com>
2024-03-02 20:55:37 +0530

    adding dark mode option

--- hearlink/src/Room.js
+++ hearlink/src/Room.js
@@ -1,13 +1,22 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
 import logoImage from "./assets/logo.png";
 import textImage from "./assets/hearlink text1.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 const Room = () => {
     const { roomID } = useParams();
     const containerRef = useRef(null);
+    const [isDarkTheme, setIsDarkTheme] = useState(false); // State to manage the theme
+
+    const toggleTheme = () => {
+        setIsDarkTheme(prevTheme => !prevTheme); // Toggle the theme
+        // Here you can dynamically change the background color of the body or a container div
+        document.body.style.backgroundColor = isDarkTheme ? "#FFFFFF" : "#0E151B";
+      };
 
     useEffect(() => {
         const meeting = async () => {
@@ -35,14 +44,29 @@ const Room = () => {
         }
     }, [roomID]);
 
+   
+
     return (
         <div>
-            <div style={{ background: "#21262C", padding: "5px " }}>
+            <div style={{ background: "#21262C", padding: "5px ", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                 <div style={{ display: "flex", alignItems: "center" }}>
                     <img src={logoImage} alt="First Image" style={{ width: "80px", height: "auto",marginLeft: "5px",padding:"2px" }} />
                     <img src={textImage} alt="Second Image" style={{ width: "360px", height: "auto", marginLeft: "5px" }} />
                 </div>
+                
             </div>
+            <div style={{ position: "fixed", bottom: "20px", left: "20px", zIndex: "999" ,borderRadius :"50px"}}>
+    <button 
+        onClick={toggleTheme} 
+        className="bg-gray-500 text-gray-800 font-semibold hover:text-white py-5 px-5 rounded-full flex items-center" 
+        style={{ cursor: "pointer" }}
+    >
+        <FontAwesomeIcon icon={isDarkTheme ? faSun : faMoon} className="theme-icon" size="2x" />
+    </button>
+</div>
+
+
+           
             <div ref={containerRef} style={{ width: "100vw", height: "calc(100vh - 117px)" }}>
                 {/* Video container will be mounted here */}
             </div>
