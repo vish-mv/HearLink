4984576dd89369ff0deca2e9ea60b9ef4300eacf
kavifernando <124521396+kavifernando@users.noreply.github.com>
2024-03-13 14:05:20 +0530

    update room.js

--- hearlink/src/Room.js
+++ hearlink/src/Room.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import "../src/index.css";
 import { FaHandPaper } from "react-icons/fa";
+import { MdRecordVoiceOver } from "react-icons/md";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 import logoLight from "./assets/Log short Dark.png";
@@ -174,8 +175,8 @@ const Room = () => {
                 <button onClick={toggleTheme} className="theme-button" style={{ cursor: "pointer", borderRadius: "50%" }}>
                     <FontAwesomeIcon icon={isDarkTheme ? faSun : faMoon} className="theme-icon" size="2x" />
                 </button>
-                <button className="buttonClass" onClick={handleButtonClick} style={{ cursor: "pointer", borderRadius: "50%", padding: "10px", marginLeft: "10px", fontSize:"8px",}}>
-                    Button A
+                <button className="buttonClass" onClick={handleButtonClick} style={{ cursor: "pointer", borderRadius: "50%", padding: "10px", marginLeft: "10px", fontSize:"15px",}}>
+                <MdRecordVoiceOver className="icon"/>
                 </button>
                 <button id="openWebcamButton" onClick={handleOpenWebcamButtonClick}><FaHandPaper  className="icon"/></button>
             </div>
