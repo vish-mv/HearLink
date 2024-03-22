73e0bdd0e2fa1b23f87fd73b44604f59f88bc9a9
Visal Munasinghe <127712545+vish-mv@users.noreply.github.com>
2024-03-23 00:28:03 +0530

    Update App js

--- hearlink/src/App.js
+++ hearlink/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/HearLink" element={<Home />} />
         <Route path="/room/:roomID" element={<Room />} />
       </Routes>
     </div>
